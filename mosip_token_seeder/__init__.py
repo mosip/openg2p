9da091aba75e915acadaf461ec91860bee4b498c
Lalith Kota <kotalalith@gmail.com>
2022-06-28 17:22:10 +0530

    FastApi base setup

---
+++ mosip_token_seeder/__init__.py
@@ -0,0 +1,33 @@
+import os
+import traceback
+from fastapi import FastAPI
+from dynaconf import Dynaconf
+
+settings = Dynaconf(settings_files=[os.path.join(__name__,"config.toml"), "/app/token_seeder.conf"], envvar_prefix="TOKENSEEDER", environments=False)
+app = FastAPI(
+    title="MOSIP Token Seeder",
+    version='0.1.0',
+)
+
+@app.get(settings.root.context_path + "ping")
+def ping():
+    return "ping"
+
+def get_current_worker_id(config):
+    if config.root.pid_grep_name=='local':
+        return
+    import subprocess
+    try:
+        pid_arr = sorted([int(a) for a in str(subprocess.check_output(['pgrep','-f',config.root.pid_grep_name]), 'UTF-8').split('\n') if a])
+        config.gunicorn.worker_id = pid_arr.index(os.getpid()) - 1
+    except:
+        config.gunicorn.worker_id = -1
+
+def get_pod_id(config):
+    config.docker.pod_id = int(config.docker.pod_name.split('-')[-1])
+
+get_current_worker_id(settings)
+get_pod_id(settings)
+
+from . import authengine
+from . import tokenseeder
---
+++ mosip_token_seeder/tokenseeder.py
@@ -0,0 +1,31 @@
+import uuid
+from . import app, settings
+from pydantic import BaseModel
+
+supported_output_types = ['json']
+supported_delivery_types = ['sync']
+
+class TokenSeederRequest(BaseModel):
+    name : str
+
+
+@app.post(settings.root.context_path + "authtoken/json")
+def authtoken_json(data : TokenSeederRequest, output : str = 'json', deliverytype : str = 'sync'):
+    if output not in supported_output_types:
+        return {
+            "message": "output type not supported"
+        }, 400
+    if deliverytype not in supported_delivery_types:
+        return {
+            "message": "delivery type not supported"
+        }, 400
+
+    txn_id = str(uuid.uuid4())
+
+    return {
+        'txnId': txn_id,
+        'data': data.dict(),
+        'input': 'json',
+        'output': output,
+        'deliverytype': deliverytype,
+    }
