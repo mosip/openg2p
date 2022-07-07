d20a11365d5b70dce3ed733ef5f210fce2e0a146
Mahammed Taheer <mohd.taheer@gmail.com>
2022-07-08 01:42:36 +0530

    Correct code structure and adding logging, exception handling.

--- mosip_token_seeder/authenticator/utils/cryptoutil.py
+++ mosip_token_seeder/authenticator/utils/cryptoutil.py
@@ -2,114 +2,151 @@ import os
 import base64
 import json
 from cryptography import x509
-from cryptography.hazmat.primitives import serialization, hashes, asymmetric, ciphers
+from cryptography.hazmat.primitives import hashes, asymmetric, ciphers, serialization
+from cryptography.hazmat.primitives.serialization import pkcs12, BestAvailableEncryption
 from jwcrypto import jwk, jws
+import logging
+import traceback
+from exceptions.authenticator_exception import AuthenticatorCryptoException, Errors
 
 class CryptoUtility:
-    def __init__(
-        self,
-        public_key_path,
-        private_key_path,
-        public_key_password,
-        private_key_password,
-        algorithm=None,
-        symmetric_key_size=256,
-        symmetric_nonce_size=128,
-        symmetric_gcm_tag_size=128,
-        **kwargs
-    ):
-        self.public_key, self.public_cert, self.public_cert_pem_bytes = CryptoUtility.ret_pub_key_from_cert(public_key_path, public_key_password)
-        self.private_key, self.private_key_jwk = CryptoUtility.ret_private_key(private_key_path, private_key_password)
 
-        if self.public_cert:
-            self.thumbprint = base64.urlsafe_b64encode(self.public_cert.fingerprint(hashes.SHA256()))
-        else:
-            self.thumbprint = None
+    def __init__(self, encrypt_config, sign_config, **kwargs):
+
+        self.logger = logging.getLogger(__name__)
+
+        self.encrypt_cert_obj = CryptoUtility._get_certificate_obj(encrypt_config.encrypt_cert_path, self.logger)
+        self.sign_private_key, self.sign_cert, self.ca_chain = CryptoUtility._get_priv_key_cert(sign_config.sign_p12_file_path, 
+                                                sign_config.sign_p12_file_password, self.logger)
+
+        self.sign_priv_key_jws = CryptoUtility._get_jwk_private_key(self.sign_private_key, sign_config.sign_p12_file_password, self.logger)
+
+        self.enc_cert_thumbprint = base64.urlsafe_b64encode(self.encrypt_cert_obj.fingerprint(hashes.SHA256()))
+
+        # Initalizing OAEP padding algorithm.
         self.asymmetric_encrypt_padding = asymmetric.padding.OAEP(
-            mgf=asymmetric.padding.MGF1(algorithm=hashes.SHA256()),
-            algorithm=hashes.SHA256(),
-            label=None
-        )
+                                                mgf=asymmetric.padding.MGF1(algorithm=hashes.SHA256()),
+                                                algorithm=hashes.SHA256(),
+                                                label=None)
 
-        self.symmetric_key_size = symmetric_key_size
-        self.symmetric_nonce_size = symmetric_nonce_size
-        self.symmetric_gcm_tag_size = symmetric_gcm_tag_size
-        self.algorithm = algorithm
-    
-    @staticmethod
-    def ret_pub_key_from_cert(key_path, key_pass):
-        if not key_path:
-            return None, None, None
-        with open(key_path, 'rb') as file:
-            if key_path.endswith('.cert') or key_path.endswith('.crt'):
-                pem_bytes = file.read()
-                cert = x509.load_pem_x509_certificate(pem_bytes)
-                return cert.public_key(), cert, pem_bytes
-            else:
-                return serialization.load_pem_public_key(file.read(), password=key_pass), None, None
-    
-    @staticmethod
-    def ret_private_key(key_path, key_pass):
-        if not key_path:
-            return None, None
-        with open(key_path, 'rb') as file:
-            pem_bytes = file.read()
-            return serialization.load_pem_private_key(pem_bytes, password=key_pass), jwk.JWK.from_pem(pem_bytes, password=key_pass)
-    
-    def asymmetric_encrypt(self, data):
-        return self.public_key.encrypt(
-            data,
-            self.asymmetric_encrypt_padding
-        )
-    
-    def symmetric_encrypt(self, data, key):
-        iv = os.urandom(int(self.symmetric_nonce_size/8))
-        encryptor = ciphers.Cipher(
-            ciphers.algorithms.AES(key),
-            ciphers.modes.GCM(iv, tag=None, min_tag_length=int(self.symmetric_gcm_tag_size/8)),
-        ).encryptor()
-        final = encryptor.update(data) + encryptor.finalize()
-        final += encryptor.tag + iv
-        return final
-    
-    def symmetric_decrypt(self, data, key):
-        nonce_size = int(self.symmetric_nonce_size/8)
-        tag_size = int(self.symmetric_gcm_tag_size/8)
-        iv = data[-nonce_size:]
-        tag = data[-(nonce_size + tag_size):-nonce_size]
-        data_without_iv = data[:-(nonce_size + tag_size)]
-        decryptor = ciphers.Cipher(
-            ciphers.algorithms.AES(key),
-            ciphers.modes.GCM(iv, tag=tag, min_tag_length=int(self.symmetric_gcm_tag_size/8)),
-        ).decryptor()
-        final = decryptor.update(data_without_iv) + decryptor.finalize()
-        return final
-    
-    def encrypt(self, data):
-        if isinstance(data, str):
-            data_bytes = data.encode('UTF-8')
-        elif isinstance(data, bytes):
-            data_bytes = data
-        else:
-            raise Exception('Unrecognised type')
-        key = os.urandom(int(self.symmetric_key_size/8))
-        data_hash = hashes.Hash(hashes.SHA256())
-        data_hash.update(data_bytes)
-        request = base64.urlsafe_b64encode(self.symmetric_encrypt(data_bytes,key)).decode('UTF-8')
-        encrypted_key = base64.urlsafe_b64encode(self.asymmetric_encrypt(key)).decode('UTF-8')
-        hmac_digest = base64.urlsafe_b64encode(self.symmetric_encrypt(data_hash.finalize().hex().upper().encode('UTF-8'),key)).decode('UTF-8')
-        return request, encrypted_key, hmac_digest
-    
-    def json_sign(self, data):
-        if not self.algorithm:
-            return None
+        self.symmetric_key_size = encrypt_config.symmetric_key_size
+        self.symmetric_nonce_size = int(encrypt_config.symmetric_nonce_size / 8)
+        self.symmetric_gcm_tag_size = int(encrypt_config.symmetric_gcm_tag_size / 8)
+        self.algorithm = sign_config.algorithm
         
-        jwstoken = jws.JWS(data.encode('UTF-8'))
-        jwstoken.add_signature(
-            self.private_key_jwk,
-            None,
-            json.dumps({'alg': self.algorithm,'x5c':[base64.encodebytes(self.public_cert_pem_bytes).decode('UTF-8')]}),
-            json.dumps({'kid': self.private_key_jwk.thumbprint()}),
-        )
-        sig = jwstoken.serialize(compact=True).split('.')
-        return sig[0] + '..' + sig[2]
+    
+    @staticmethod
+    def _get_certificate_obj(cert_path, logger):
+        # will add not 'None' validation in DynaConf validation API.
+        logger.info('Creating certificate Object for the file Path: {}'.format(cert_path))
+        try:
+            with open(cert_path, 'rb') as file:
+                cert = x509.load_pem_x509_certificate(file.read())
+                logger.info('Cretificate Object Creation successful(PEM Format).')
+                return cert
+        except:
+            exp = traceback.format_exc() 
+            logger.error('Error Reading PEM file format. Error Message: {}'.format(exp))
+
+        try:
+            with open(cert_path, 'rb') as file:
+                cert = x509.load_der_x509_certificate(file.read())
+                logger.info('Cretificate Object Creation successful(DER Format).')
+                return cert
+        except:
+            exp = traceback.format_exc()
+            logger.error('Error Reading both PEM/DER file format. Error: {}'.format(exp))
+            raise AuthenticatorCryptoException(Errors.AUT_CRY_001.name, Errors.AUT_CRY_001.value.format(cert_path))
+
+    @staticmethod
+    def _get_priv_key_cert(p12_file_path, p12_file_pass, logger):
+       
+        logger.info('Reading P12 file. File Path: {}'.format(p12_file_path))
+        try: 
+            with open(p12_file_path, 'rb') as file:
+                pem_bytes = file.read()
+                return pkcs12.load_key_and_certificates(pem_bytes, bytes(p12_file_pass, 'utf-8'))
+        except:
+            exp = traceback.format_exc()
+            logger.error('Error Loading P12 file to create objects. Error: {}'.format(exp))
+            raise AuthenticatorCryptoException(Errors.AUT_CRY_002.name, Errors.AUT_CRY_002.value.format(p12_file_path))    
+    
+    @staticmethod
+    def _get_jwk_private_key(priv_key_obj, key_password, logger):
+
+        key_pwd_bytes = bytes(key_password, 'utf-8')
+        priv_key_pem = priv_key_obj.private_bytes(encoding=serialization.Encoding.PEM, format=serialization.PrivateFormat.PKCS8,
+                            encryption_algorithm=BestAvailableEncryption(key_pwd_bytes))
+        logger.info('Creating JWK key for JWS signing.')
+        return jwk.JWK.from_pem(priv_key_pem, password=key_pwd_bytes)
+        
+    def _asymmetric_encrypt(self, aes_random_key):
+        self.logger.debug('Encrypting the AES Random Key.')
+        public_key_obj = self.encrypt_cert_obj.public_key()
+        return public_key_obj.encrypt(aes_random_key, self.asymmetric_encrypt_padding)
+    
+    def _symmetric_encrypt(self, data, key):
+        self.logger.debug('Encrypting the Auth Data using AES Key.')
+        iv = os.urandom(self.symmetric_nonce_size)
+        aes_encryptor_obj = ciphers.Cipher(ciphers.algorithms.AES(key),
+                                   ciphers.modes.GCM(iv, tag=None, min_tag_length=self.symmetric_gcm_tag_size)).encryptor()
+
+        enc_data = aes_encryptor_obj.update(data) + aes_encryptor_obj.finalize()
+        enc_data_final = enc_data + aes_encryptor_obj.tag + iv
+        return enc_data_final
+    
+    def encrypt_auth_data(self, auth_data):
+        self.logger.info('Request for Auth Data Encryption.')
+        if isinstance(auth_data, str):
+            auth_data_bytes = auth_data.encode('UTF-8')
+        elif isinstance(auth_data, bytes):
+            auth_data_bytes = auth_data
+        else:
+            raise ValueError('Unrecognised type')
+        
+        try:
+            # Generate a random AES Key and encrypt Auth Request Data using the generated random key.
+            aes_key = os.urandom(int(self.symmetric_key_size/8))
+            encrypted_auth_data = self._symmetric_encrypt(auth_data_bytes, aes_key)
+            encrypted_auth_b64_data = base64.urlsafe_b64encode(encrypted_auth_data).decode('UTF-8')
+            self.logger.info('Generating AES Key and encrypting Auth Data Completed.')
+
+            # Encrypt the random generated key using the IDA partner certificate received from IDA server.
+            encrypted_aes_key = self._asymmetric_encrypt(aes_key)
+            encrypted_aes_key_b64 = base64.urlsafe_b64encode(encrypted_aes_key).decode('UTF-8')
+            self.logger.info('Encrypting Random AES Key Completed.')
+
+            # Generate SHA256 for the Auth Request Data 
+            sha256_hash_obj = hashes.Hash(hashes.SHA256())
+            sha256_hash_obj.update(auth_data_bytes)
+            auth_data_hash = sha256_hash_obj.finalize().hex().upper().encode('UTF-8')
+            enc_auth_data_hash = self._symmetric_encrypt(auth_data_hash, aes_key)
+            enc_auth_data_hash_b64 = base64.urlsafe_b64encode(enc_auth_data_hash).decode('UTF-8')
+            self.logger.info('Generation of SHA256 Hash for the Auth Data completed.')
+
+            return encrypted_auth_b64_data, encrypted_aes_key_b64, enc_auth_data_hash_b64
+        except:
+            exp = traceback.format_exc()
+            self.logger.error('Error encrypting Auth Data. Error Message: {}'.format(exp))
+            raise AuthenticatorCryptoException(Errors.AUT_CRY_003.name, Errors.AUT_CRY_003.value)
+            
+    
+    def sign_auth_request_data(self, auth_request_data):
+        self.logger.info('Request for Sign Auth Request Data.')
+        try:
+            jws_object = jws.JWS(auth_request_data.encode('UTF-8'))
+            jws_object.add_signature(
+                self.sign_priv_key_jws,
+                None,
+                json.dumps({'alg': self.algorithm,
+                            'x5c':[base64.encodebytes(self.sign_cert.public_bytes(encoding=serialization.Encoding.PEM)).decode('UTF-8')]}), # Protected header attributes, is IDA checking for protected attributes?
+                json.dumps({'kid': base64.encodebytes(self.sign_cert.fingerprint(hashes.SHA256())).decode('UTF-8')}) # UnProtected Header attributes.
+            )
+
+            jws_signature = jws_object.serialize(compact=True).split('.')
+            self.logger.info('Generation for JWS Signature completed.')
+            return jws_signature[0] + '..' + jws_signature[2]
+        except:
+            exp = traceback.format_exc()
+            self.logger.error('Error Signing Auth Data. Error Message: {}'.format(exp))
+            raise AuthenticatorCryptoException(Errors.AUT_CRY_004.name, Errors.AUT_CRY_004.value)
