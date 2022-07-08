405676db6a2186c5da4b573aeea238b1b89bba2f
Lalith Kota <kotalalith@gmail.com>
2022-07-08 15:56:49 +0530

    [MOSIP-22680] Added password printing for debug

--- mosip_token_seeder/app.py
+++ mosip_token_seeder/app.py
@@ -8,6 +8,8 @@ config = mosip_token_seeder.init_config()
 app = mosip_token_seeder.init_app(config)
 logger = mosip_token_seeder.init_logger(config)
 
+if config.db.print_password_on_startup:
+    logger.info('DB Password: %s' % config.db.password)
 mosip_authenticator = authenticator.initialize(config, logger)
 token_seeder = tokenseeder.initialize(config, logger)
 authtokenapi.initialize(app, config, logger, token_seeder.request_id_queue)
